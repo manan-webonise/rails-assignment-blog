d6935b81d0255fcdfcccf0d1d35b4d303ccd779c
Manan <manan.chhabra@weboniselab.com>
2021-10-07 14:41:27 +0530

    Publish / Unpublish with HyperLinks

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(title:"...", body:"...")
+  
+    if @post.save
+      redirect_to @post
+    else  
+      render :new 
+    end
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  get '/posts', to: 'posts#index'
+  #get '/posts', to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get '/posts/:id', to: 'posts#show'
+  resources :posts
 end
